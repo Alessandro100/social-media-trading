94e635a3bf99531abd32dac5d6587ad732c5d712
Nick Zombolas <nickzombolas@gmail.com>
2020-01-26 17:03:40 -0500

    more added to profile page

--- clients/src/Components/ProfilePage.js
+++ clients/src/Components/ProfilePage.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import Header from './Header'
 import '../App.css';
 import ProfileHeader from './ProfileHeader';
+import Graph from './Graph';
+import Positions from './Positions';
 
 export class ProfilePage extends Component {
     render() {
+        const { transactions, positions, followers } = this.props.state
+        console.log('positions')
+        console.log(positions)
         return (
             <>
             <Header />
                 <div className='profile-container'>
                     <div className='profile-main'>
                         <ProfileHeader />
+                        <Graph />
+                        <Positions positions={positions} />
                     </div>
                     <div className='profile-sidebar'>
                         sidebar
