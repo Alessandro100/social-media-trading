0b150b26d5bda5c40e462d2f3f05a7c32eb4370c
Alessandro <kalessandro14@gmail.com>
2020-01-26 12:57:54 -0500

    setup frontend communication with backend

--- clients/src/App.js
+++ clients/src/App.js
@@ -2,36 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from './Components/Header'
+import TransactionService from './Services/transaction'
 
 class App extends Component {
-  // axios.get(`http://127.0.0.1:5000/user`, {
-  //   params: {
-  //     username: 'test'
-  //   }
-  // })
-  // .then(res => {
-  //       console.log("GET success")
-  //       console.log(res);
-  //       console.log(res.data);
-  // })
-
-  // axios.post(`http://127.0.0.1:5000/user`, {username: 'test user from react'}).then(res => {
-  //       console.log("connection success")
-  //       console.log(res);
-  // })
-
   constructor(){
     super()
     this.state = {
     }
-    this.goToAlpaca = this.goToAlpaca.bind(this);
-    this.buyStock = this.buyStock.bind(this);
-    this.registerUser = this.registerUser.bind(this);
     this.registerAlpaca = this.registerAlpaca.bind(this);
-    this.getUserPositions = this.getUserPositions.bind(this);
-    this.getUserTransactions = this.getUserTransactions.bind(this);
-    this.addFollower = this.addFollower.bind(this);
-    this.getUserFeed = this.getUserFeed.bind(this);
+    this.goToAlpaca = this.goToAlpaca.bind(this);
   }
 
   USERNAME = 'test';
@@ -42,36 +21,12 @@ class App extends Component {
     window.location.href = url2;
   }
 
-  registerUser() {
-    var username = this.USERNAME
-    var password = '1234'
-    axios.post(`http://127.0.0.1:5000/user`, {username: username, password: password}).then(res => {
-      console.log("token success")
-      console.log(res);
-    })
-  }
-
-  buyStock() {
-    console.log("buy stock");
-    var params ={
-      username: this.USERNAME,
-      symbol: 'msft',
-      action: 'buy',
-      quantity: '3'
-    }
-    axios.post(`http://127.0.0.1:5000/alpaca-transaction`, params).then(res => {
-      console.log("BOUGHT APPLE")
-      console.log(res);
-    })
-  }
-
   //make sure code is in url
   registerAlpaca() {
-    console.log("helloooooooo");
-    var x = this.getUrlVars();
-    console.log(x);
-    if(x && x['code']) {
-      var code = x['code']
+    //wait -> login logic needs to be fixed
+    var urlVars = this.getUrlVars();
+    if(urlVars && urlVars['code']) {
+      var code = urlVars['code']
       console.log("AN ALPACA CODE EXISTS")
       axios.post(`http://127.0.0.1:5000/alpaca-registration`, {username: this.USERNAME, code: code}).then(res => {
         console.log("token success")
@@ -88,55 +43,15 @@ class App extends Component {
     return vars;
   }
 
-  getUserPositions() {
-    console.log("calling position list: " + 'http://127.0.0.1:5000/position-list')
-    axios.get(`http://127.0.0.1:5000/position-list`, {params: { username: this.USERNAME}}).then(res => {
-          console.log("GET success")
-          console.log(res);
-          console.log(res.data);
-    })
-  }
-
-  getUserTransactions() {
-    console.log("calling position list: " + 'http://127.0.0.1:5000/transaction-list')
-    axios.get(`http://127.0.0.1:5000/transaction-list`, {params: { username: this.USERNAME}}).then(res => {
-          console.log("GET success")
-          console.log(res);
-          console.log(res.data);
-    })
-  }
-
-  addFollower() {
-    console.log("calling position list: " + 'http://127.0.0.1:5000/follow')
-    axios.post(`http://127.0.0.1:5000/follow`, {username: this.USERNAME, username_to_follow: 'alex_test'}).then(res => {
-      console.log("token success")
-      console.log(res);
-    })
-  }
-
-  getUserFeed(){
-    console.log("calling position list: " + 'http://127.0.0.1:5000/feed')
-    //this.USERNAME
-    axios.get(`http://127.0.0.1:5000/feed`, {params: { username: 'test'}}).then(res => {
-          console.log("GET success")
-          console.log(res);
-          console.log(res.data);
-    })
-  }
 
   render(){
 
     return (
       <div className="App">
         <Header />
+        <h1>Ignore my buttons</h1>
         <button onClick={this.goToAlpaca}>Log into Alpaca Button</button>
         <button onClick={this.registerAlpaca}>Save Alpaca Code (check uri)</button>
-        <button onClick={this.buyStock}>Buy Stock</button>
-        <button onClick={this.registerUser}>Register User</button>
-        <button onClick={this.getUserPositions}>User Position Test</button>
-        <button onClick={this.getUserTransactions}>User Transaction Test</button>
-        <button onClick={this.addFollower}>AddFollower Test</button>
-        <button onClick={this.getUserFeed}>getUserFeed Test</button>
       </div>
     );
   }
