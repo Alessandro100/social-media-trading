11536a0b3be096834e98f16064f7f9ebcb8469c9
Nick Zombolas <nickzombolas@gmail.com>
2020-01-26 16:32:25 -0500

    profile header added

--- clients/src/Components/ProfilePage.js
+++ clients/src/Components/ProfilePage.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import '../App.css';
+import ProfileHeader from './ProfileHeader';
 
 export class ProfilePage extends Component {
     render() {
         return (
             <>
             <Header />
-                <div>This is the profile page</div>
+                <div className='profile-container'>
+                    <div className='profile-main'>
+                        <ProfileHeader />
+                    </div>
+                    <div className='profile-sidebar'>
+                        sidebar
+                    </div>
+                </div>
             </>
         )
     }
