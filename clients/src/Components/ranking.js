6d0987d9426a439d16383ab76961317d9b9f1966
Alessandro <kalessandro14@gmail.com>
2020-01-30 09:57:11 -0500

    got fixes

---
+++ clients/src/Components/ranking.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+export default class ranking extends Component {
+    render() {
+        
+        return (
+            <div className="leaderboard-container"> <h3 className="">Leaderboard</h3>
+                <table> <th>Weekly Rank: {this.props.ranking.weekRank} </th>
+               <th> Percentile: {this.props.ranking.percentile}</th>
+                </table>
+               
+            </div>
+        )
+    }
+}
